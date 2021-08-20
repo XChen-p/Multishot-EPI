02a370062d9287c9c472d2a2aef4f166bece28e8
Xi Chen <57924006+XChen-p@users.noreply.github.com>
2021-08-20 17:40:59 +0100

    Add files via upload

---
+++ pinv_hankel.m
@@ -0,0 +1,33 @@
+function [M,N] = pinv_hankel(H, k, Nx, Ny, Nc,Nshot)
+    M = zeros(Nx*Ny, Nc,Nshot);
+    N = zeros(Nx*Ny, Nc,Nshot);
+    H = reshape(H,Nx-(k-1),[],k,k,Nc,Nshot);
+    for ii = 1:size(H,2)
+        for jj = 1:k
+            for kk = 1:k
+                idx = ((ii-1)*Nx+1:(ii-1)*Nx+Nx-(k-1))+(jj-1)+(kk-1)*Nx;
+                M(idx,:,:) = M(idx,:,:) + permute(H(:,ii,jj,kk,:,:),[1,5,6,2,3,4]);
+                N(idx,:,:) = N(idx,:,:) + 1;
+            end
+        end
+    end
+    M = reshape(M./N, Nx,Ny,Nc,Nshot);    
+    N = reshape(N, Nx,Ny,Nc,Nshot); 
+end
+% function [M, N] = pinv_hankel(H, k, Nx, Ny, Ns)
+%     M = zeros(Nx*Ny, Ns);
+%     N = zeros(Nx*Ny, Ns);
+%     H = reshape(H,Nx-(k-1),[],k,k,Ns);
+%     for ii = 1:size(H,2)
+%         for jj = 1:k
+%             for kk = 1:k
+%                 idx = ((ii-1)*Nx+1:(ii-1)*Nx+Nx-(k-1))+(jj-1)+(kk-1)*Nx;
+%                 M(idx,:) = M(idx,:) + squeeze(H(:,ii,jj,kk,:));
+%                 N(idx,:) = N(idx,:) + 1;
+%             end
+%         end
+%     end
+%           
+%     M = reshape(M./N, Nx,Ny,Ns);    
+%     N = reshape(N, Nx,Ny,Ns); 
+% end
