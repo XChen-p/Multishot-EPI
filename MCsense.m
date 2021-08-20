9eddb238409988c55307554d4bdaaa2f74c9e51f
Xi Chen <57924006+XChen-p@users.noreply.github.com>
2021-08-20 17:29:53 +0100

    Update MCsense.m

--- MCsense.m
+++ MCsense.m
@@ -10,29 +10,20 @@ para.sens=sense;
     
     para.y   =   reshape((1/2)*M_adj(k_obj,para),[],1);
     %pcg
-    [im,~]   =   pcg(@(x)cgfun(x, para), para.y, 1E-6, 1000);
-%      k      =   fftdim(reshape(im, para.kx, para.ky),1:2);
-   im       =   abs(reshape(im, para.kx, para.ky));
+    [im,~]   =   pcg(@(x)cgfun(x, para), para.y, 1E-6, 100);
+    im       =   abs(reshape(im, para.kx, para.ky));
      
     
 end
 
 function k_mc = M_fwd(im_sc, para)
-% S*F*Sens
- 
  
 im_mc = repmat(im_sc,[1,1,para.coil,1]).*para.sens;
 k_mc  = fftdim(im_mc,1:2).*para.sample;
 
-% k_mc=(F*im_mc).*para.sample;
-
 end
+
 function im_sc = M_adj(k_mc, para)
-%  sens*F*S
-% k_mc=k_mc.*para.sample;
-% for i=1:par.coil
-%     im_mc(:,:,i)=par.FT'*k_mc(:,:,i);
-% end
 
 im_mc = ifftdim(k_mc.*para.sample,1:2);%n*l*coil*shot
 im_sc = sum(im_mc.*conj(para.sens),3);%n*l*1*shot
@@ -40,7 +31,7 @@ im_sc = sum(im_mc.*conj(para.sens),3);%n*l*1*shot
 end
 
 function q = cgfun(x, para)
-% S*F*Sens*
+
 x = reshape(x, para.kx, para.ky); 
 q = 1/2*M_adj(M_fwd(x,para),para) ;
 q = reshape(q,[],1);
