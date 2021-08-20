c16c16e75170e64f04a2fc98a4715cf0c1bb603e
Xi Chen <57924006+XChen-p@users.noreply.github.com>
2021-08-20 17:42:57 +0100

    Update pinv_hankel.m

--- pinv_hankel.m
+++ pinv_hankel.m
@@ -14,20 +14,3 @@ function [M,N] = pinv_hankel(H, k, Nx, Ny, Nc,Nshot)
     M = reshape(M./N, Nx,Ny,Nc,Nshot);    
     N = reshape(N, Nx,Ny,Nc,Nshot); 
 end
-% function [M, N] = pinv_hankel(H, k, Nx, Ny, Ns)
-%     M = zeros(Nx*Ny, Ns);
-%     N = zeros(Nx*Ny, Ns);
-%     H = reshape(H,Nx-(k-1),[],k,k,Ns);
-%     for ii = 1:size(H,2)
-%         for jj = 1:k
-%             for kk = 1:k
-%                 idx = ((ii-1)*Nx+1:(ii-1)*Nx+Nx-(k-1))+(jj-1)+(kk-1)*Nx;
-%                 M(idx,:) = M(idx,:) + squeeze(H(:,ii,jj,kk,:));
-%                 N(idx,:) = N(idx,:) + 1;
-%             end
-%         end
-%     end
-%           
-%     M = reshape(M./N, Nx,Ny,Ns);    
-%     N = reshape(N, Nx,Ny,Ns); 
-% end
