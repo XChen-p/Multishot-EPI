2976f614204c9cdb189cda6ea6966a2b19dfe6b0
Xi Chen <57924006+XChen-p@users.noreply.github.com>
2022-09-27 21:09:18 +0100

    Update CAIPI_Sampling.m

--- CAIPI_Sampling.m
+++ CAIPI_Sampling.m
@@ -36,11 +36,11 @@ shift_1=1;
 
 %   shift_2 is the "within block" shift
 %golden ratio
-shift_2 =   round(width*(sqrt(5)-1)/2);
-while lcm(shift_2, width) ~= shift_2*width
-    shift_2 = shift_2 + 1;
-end
-% shift_2=dims(3)/dims(4);
+%shift_2 =   round(width*(sqrt(5)-1)/2);
+%while lcm(shift_2, width) ~= shift_2*width
+%    shift_2 = shift_2 + 1;
+%end
+shift_2=dims(3)/dims(4);
 
 
 
@@ -55,14 +55,14 @@ for t = 1:dims(4)
     
     idx_s =   floor((t-1)/(num_block));
     
-    y0  =   mod(t-1, R);
+    % y0  =   mod(t-1, R);
+    y0 = 0;
     %     y0  =   mod(mod(t-1,num_block), R);%no difference if mod(num_block,R)=0
     z0  =   mod(shift_1*(t-1), num_block)*width;
     b0  =   mod(shift_2*idx_s, width);
     
     for i = ceil(dims(2)*(1-pf_y))+1:R:dims(2)
-        %         ii = (ceil(i/R)-1)*slope+1;%
-        ii=(i-1)*slope+1;
+        ii = (ceil(i/R)-1)*slope+1;%
         mask(:, y0 + i, z0 + mod(b0 + ii - 1, width) + 1, t) =  true;
     end
     if verbose==1
